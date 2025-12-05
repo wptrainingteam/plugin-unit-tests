dcd15cb8e4761d4166ca43b0fa3a51843514818c
David Perez <david@closemarketing.es>
2025-12-05 17:08:33 +0100

    Update plugin-unit-tests.php

--- plugin-unit-tests.php
+++ plugin-unit-tests.php
@@ -33,3 +33,4 @@ define( 'PLUNIT_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'PLUNIT_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 
 require_once PLUNIT_PLUGIN_PATH . 'includes/functions-to-test.php';
+require_once PLUNIT_PLUGIN_PATH . 'includes/register-cpt-book.php';
